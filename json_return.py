29fdcec67f6e05bed527daa7fa701b6373cbaa2f
Kingsley <kingsley.low@gmail.com>
2022-06-20 22:52:50 +0800

    Added error json return and new query string

--- json_return.py
+++ json_return.py
@@ -1,10 +1,14 @@
 class json_return:
-    
-    def __init__(self, total_cost, avg_cost_wo_gst, top_supplier_country):
-        self.ret_json = {"totalCost": total_cost,
-                         "avgCostWoGst": avg_cost_wo_gst,
-                         "topSupplierCountry": top_supplier_country
-                        }
+
+    def __init__(self, total_cost, avg_cost_wo_gst, top_supplier_country, error_code=0):
+        if error_code == 0:
+            self.ret_json = {"totalCost": total_cost,
+                             "avgCostWoGst": avg_cost_wo_gst,
+                             "topSupplierCountry": top_supplier_country
+                            }
+        elif error_code == -1:
+            self.ret_json = {"error": "No data within this date range"
+                            }
 
     def get_json(self):
         return self.ret_json
--- list_of_var.py
+++ list_of_var.py
@@ -13,10 +13,28 @@ TABLE_NAME = "transaction_details"
 SQL_URL = "mysql+pymysql://{user}:{pw}@{host}".format(host=HOSTNAME,
                                                       user=UNAME,
                                                       pw=PWD)
+USE_DB = "USE " + DB_NAME
+
 # Database Initialisation Query
 INIT_QUERY = ["DROP DATABASE IF EXISTS {}",
               "CREATE DATABASE {}",
-              "USE {}"]
+              USE_DB]
+
+# Query
+SQL_QUERY = """
+SELECT 
+SUM(`Total incl GST`) AS `totalCost`,
+AVG(`Unitprice - SGD`) AS `avgCostWoGst`,
+`Country of origin` AS `topSupplierCountry`
+FROM `transaction_details`
+WHERE
+`Transaction date` >= '{}'
+AND
+`Transaction date` < '{}'
+GROUP BY`Country of origin`
+ORDER BY Count(`Country of origin`) DESC
+LIMIT 1;
+"""
 
 # Excel Path
 EXCEL_PATH = "./excel_input/SaladStopdummydata.xlsx"
