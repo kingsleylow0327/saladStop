9ddb062fa2dcf34c33e76c380ff6cc86795e7ade
Kingsley <kingsley.low@gmail.com>
2022-06-21 00:13:08 +0800

    Error Mapping

---
+++ errors.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class error(Enum):
+    OUT_OF_RANGE = -1
+    MISSING_PARAM = -2
+    WRONG_FORMAT = -3
+
+ERROR_MAP = {error.OUT_OF_RANGE: {"Error": "No data within this date range"},
+             error.MISSING_PARAM: {"Error": "Missing startDate or endDate"},
+             error.WRONG_FORMAT: {"Error": "Wrong date format(YYYY-MM-DD) or Invalid Date"}
+            }
