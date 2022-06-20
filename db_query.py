10b665efed158e03e013b1609b31657936085b4a
Kingsley <kingsley.low@gmail.com>
2022-06-21 00:14:34 +0800

    Error Handling

--- db_query.py
+++ db_query.py
@@ -1,7 +1,9 @@
+import datetime
 import pandas as pd
+from errors import error
 from json_return import json_return
-from sqlalchemy import create_engine
 from list_of_var import SQL_URL, DB_NAME, TABLE_NAME, INIT_QUERY, USE_DB, SQL_QUERY
+from sqlalchemy import create_engine
 
 # Create SQLAlchemy engine to connect to MySQL Database
 engine = create_engine(SQL_URL)
@@ -16,13 +18,24 @@ def init_database_from_excel(path):
     return df
 
 
-def get_data(start_date, end_date):
+def get_query_data(start_date, end_date):
+    # Missing Date
+    if not start_date or not end_date:
+        return json_return(*(0,0,0), error_code=error.MISSING_PARAM).get_json()
+    
+    # Format Check
+    try:
+        datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    except:
+        return json_return(*(0,0,0), error_code=error.WRONG_FORMAT).get_json()
+
     engine.execute(USE_DB)
     sql_ret = engine.execute(SQL_QUERY.format(start_date, end_date)).fetchmany()
 
     # Out of date range
     if len(sql_ret) != 1:
-        return json_return(*(0,0,0), error_code=-1).get_json()
+        return json_return(*(0,0,0), error_code=error.OUT_OF_RANGE).get_json()
 
     return json_return(*sql_ret[0]).get_json()
 
