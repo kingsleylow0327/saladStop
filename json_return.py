2649baad47098f5d2c3b676b8896b741b5c33737
Kingsley <kingsley.low@gmail.com>
2022-06-21 00:15:22 +0800

    Adapted to code error

--- json_return.py
+++ json_return.py
@@ -1,14 +1,15 @@
-class json_return:
+import errors
+import json
 
+class json_return:
     def __init__(self, total_cost, avg_cost_wo_gst, top_supplier_country, error_code=0):
         if error_code == 0:
             self.ret_json = {"totalCost": total_cost,
-                             "avgCostWoGst": avg_cost_wo_gst,
-                             "topSupplierCountry": top_supplier_country
+                              "avgCostWoGst": avg_cost_wo_gst,
+                              "topSupplierCountry": top_supplier_country
                             }
-        elif error_code == -1:
-            self.ret_json = {"error": "No data within this date range"
-                            }
+        else:
+            self.ret_json = errors.ERROR_MAP[error_code]
 
     def get_json(self):
         return self.ret_json
